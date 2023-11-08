01acf09e440a16dfb9e010f2ce1b70ec6cf5f9c7
Aliceonly <aliceonly@foxmail.com>
2023-11-08 15:47:18 +0800

    update

---
+++ packages/nextjs/pages/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import type { NextPage } from "next";
+import { MetaHeader } from "~~/components/MetaHeader";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5 w-[90%] md:w-[75%]">
+          <h1 className="text-center mb-6">
+            <span className="block text-2xl mb-2">NFT MARKET</span>
+            <span className="block text-4xl font-bold">TZY: Simple NFT</span>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
